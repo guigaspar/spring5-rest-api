56509df93b249982a74fa0c7460c66cf9d3f9c35
Guilherme Gaspar <gui.gaspar92@gmail.com>
2018-01-08 01:22:02 -0200

    Adding Swagger 2

--- src/main/java/guigaspar/development/spring5restapi/controller/CustomerController.java
+++ src/main/java/guigaspar/development/spring5restapi/controller/CustomerController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import guigaspar.development.spring5restapi.model.CustomerDTO;
 import guigaspar.development.spring5restapi.model.CustomerListDTO;
 import guigaspar.development.spring5restapi.services.CustomerService;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * @author Guilherme Gaspar - 03/01/2018
@@ -31,30 +32,35 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @ApiOperation(value = "Get a List of Customers")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public CustomerListDTO getListOfCustomers(){
         return new CustomerListDTO(customerService.getAllCustomers());
     }
 
+    @ApiOperation(value = "Get Customer by ID")
     @GetMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public CustomerDTO getCustomerById(@PathVariable String id){
         return customerService.getCustomerById(id);
     }
 
+    @ApiOperation(value = "Create a new customer")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CustomerDTO createNewCustomer(@RequestBody CustomerDTO customerDTO){
         return customerService.createNewCustomer(customerDTO);
     }
 
+    @ApiOperation(value = "Update Customer")
     @PutMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public CustomerDTO updateCustomer(@PathVariable String id, @RequestBody CustomerDTO customerDTO){
         return customerService.saveCustomerByDTO(id, customerDTO);
     }
 
+    @ApiOperation(value = "Delete Customer")
     @DeleteMapping({"/{id}"})
     @ResponseStatus(HttpStatus.OK)
     public void deleteCustomer(@PathVariable String id){
